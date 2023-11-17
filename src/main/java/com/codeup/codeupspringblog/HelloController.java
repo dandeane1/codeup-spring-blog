aa1961ace2aacf335458b60dd8ad0e10eb469b12
Daniel Deane <daniel.michael.deane@gmail.com>
2023-11-17 16:53:11 -0600

    spring day 2 having issues with local 8080 again

--- src/main/java/com/codeup/codeupspringblog/HelloController.java
+++ src/main/java/com/codeup/codeupspringblog/HelloController.java
@@ -1,27 +1,32 @@
 package com.codeup.codeupspringblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-class HelloController {
+
+    @Controller
+    public class HelloController {
+
+        @GetMapping("/hello")
+        public String hello() {
+            return "hello";
+        }
+
+        @GetMapping("/hello/{name}")
+        public String sayHello(@PathVariable String name, Model model) {
+            model.addAttribute("name", name);
+
+            return "hello";
+        }
+
+        @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+        @ResponseBody
+        public String addOne(@PathVariable int number) {
+            return number + " plus one is " + (number + 1) + "!";
+        }
 
 
 
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello from Spring!";
-    }
-    @GetMapping("/hello/{name}")
-    @ResponseBody
-    public String sayHello(@PathVariable String name) {
-        return "Hello " + name + "!";
-    }
 
-    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
-    @ResponseBody
-    public String addOne(@PathVariable int number) {
-    return number + " plus one is " + (number + 1)+ "!";
-    }
 }
